a7a32bd5981a7db1c8b2726b7471c30f0e167724
Ali Eren <ali-imran7@live.de>
2022-01-28 08:40:20 +0100

    a

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -11,7 +11,7 @@ public class Meteor : MonoBehaviour
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed, 0) * Time.timeScale;
+        rb.velocity = new Vector2(-speed, 0) ;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
        
     }
